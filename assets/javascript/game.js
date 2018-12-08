c7d266b32691090344605a86629e3610846df12f
asl01 <alakoff@gmail.com>
2018-12-07 23:01:27 -0500

    Add latest code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,113 +2,184 @@
 var game = {
 
     //Game variables
-    wins: 0,
-    losses: 0,
     unanswered:0,
-    qTimer:0,
+    qTime:0,
+    dTime:0,
     response:'',
     qInterval:0,
-    qNum:'',
+    dInterval:0,
+    qNum:0,
+    correctCounter:0,
+    wrongCounter:0,
+    answer:'',
 
-    //Question arrays
-    // q1: ["What year was 1980?","1980","1981","1982","1983","1980"],
-    // q2: ["What year was 1981?","1980","1981","1982","1983","1981"],
-    // q3: ["What year was 1982?","1980","1981","1982","1983","1982"],
-    // q4: ["What year was 1983?","1980","1981","1982","1983","1983"],
-
-    //Question numbers
-    qArray: [["What year was 1980?","1980","1981","1982","1983","1980"],["What year was 1981?","1980","1981","1982","1983","1981"]],
-
+    //Questions array
+    qArray: [
+        ["Which 1980's US president survived an assassination attempt?","Bill Clinton","Ronald Reagan","George H W Bush","Jimmy Carter","Ronald Reagan"],
+        ["In 1989, who had the first of many No 1s songs with Straight Up?","Paul Anka","Bangles","Johnny Cash","Paula Abdul","Paula Abdul"],
+        ["Where in the Ukraine was there a nuclear explosion in 1986?","Chernobyl",
+        "Odessa","Donetsk","Kiev","Chernobyl"],
+        ],
 
     //Timer function
-    timer: function() {
+    qTimer: function() {
 
         //Decrease question timer by one second
-        game.qTimer--;
+        game.qTime--;
 
         //Show the number in the timer
-        $(".timer").html("<h4>" + "Time Remaining :" + game.qTimer + "</h4>");
+        $(".timer").html("<h4>" + "Time Remaining :" + game.qTime + "</h4>");
 
-        if (game.response !== ''){
-            console.log("timer stopped line 27");
+        if (game.qTime === 0) {
             clearInterval(game.qInterval);
-            game.qloop();
-        }
-
-        if (game.qTimer === 0) {
-            console.log("timer stopped line 32");
-            game.qTimer=0;
+            game.qTime = 0;
             game.unanswered++;
-            clearInterval(game.qInterval);
-            game.qloop();
+            game.gameOver();
         }
     },
 
-    checkResponse: function(answer,response) {
-        console.log(answer,response);
+    dTimer: function() {
+        //Decrease right/wrong display timer by one second
+        game.dTime--;
+
+        //When dTime is zero, clear dTimer
+        if (game.dTime === 0) {
+            clearInterval(game.dInterval);
+        }
+    },
+
+    //Checks for correct or wrong answer
+    checkResponse: function() {
+
+        if (game.answer === game.response) {
+            game.correctCounter++;
+            $(".qmessage").css("background","green").text("You are correct!");
+            game.dTime = 2;
+            setInterval(game.dTimer, 1000);
+        } else {
+            game.wrongCounter++;
+            $(".qmessage").css("background","red").text("You are NOT correct!");
+            game.dTime = 2;
+            setInterval(game.dTimer, 1000);
+        }
+        game.gameOver();
     },
 
 
-    qloop: function() {
-        game.qnum = 0;
-        while (game.qnum < game.qArray.length){
-            if (game.qInterval===0) {
-                game.questions(game.qnum);
-                game.qnum++;
+    //Checks to see if all game questions have been asked
+    //Show scores if all questions are done
+    gameOver: function() {
+
+        //Add one to question number
+        game.qNum++;
+
+        console.log("game over: " + game.qNum);
+        console.log("array length: " + game.qArray.length);
+
+        if (game.qNum === game.qArray.length) {
+            console.log("qNum equal to array length - Game Done");
+            $(".qmessage").css("background","lightgray");
+            $(".qmessage").text("The test has finished");
+            game.showScore();
+        } else if (game.qNum < game.qArray.length) {
+                console.log("qNum " + game.qNum + " qArrayLength: " + game.qArray.length);
+                game.questions(game.qNum);
             }
-        }
     },
 
+    //Shows scoreboard div and enters values for the counters
+    showScore: function() {
 
+        //Show scores display with counter information
+        $(".scores").css("display","block");
+        $(".correct").text("Correct Answers: " + game.correctCounter);
+        $(".wrong").text("Wrong Answers: " + game.wrongCounter);
+        $(".unanswered").text("Unanswered Questions: " + game.unanswered);
+
+        //If test again button is clicked
+        $(".btn-playagain").on("click",function() {
+            game.play();
+        })
+
+        //If no thanks  button is clicked
+        $(".btn-noplay").on("click",function() {
+
+            //Clear any running timers
+            clearInterval(game.qInterval);
+            clearInterval(game.dInterval);
+
+            //Hide scores display
+            $(".scores").css("display","none");
+
+            //Hide question display
+            $(".qa").css("display","none");
+        })
+    },
+
+    //Question display and response processing
     questions: function(i) {
 
-        //Set 10 seconds per question
-        game.qTimer = 10;
+        console.log("current question num: " + i);
 
-        console.log(game.qArray);
-        console.log(i);
+        //Reset question correct or wrong message display
+        $(".qmessage").css("background","lightgray");
+        $(".qmessage").text("Good Luck !");
+
+        //Set 25 seconds per question
+        game.qTime = 10;
 
         //Update question and possible answers
         $(".question").text(game.qArray[i][0]);
-        // console.log(game.qArray[i]);
         $(".a1").text(game.qArray[i][1]);
         $(".a2").text(game.qArray[i][2]);
         $(".a3").text(game.qArray[i][3]);
         $(".a4").text(game.qArray[i][4]);
-        var answer = game.qArray[i][5];
-
+        game.answer = game.qArray[i][5];
 
         //Start question timer function to run every second
-        game.qInterval = setInterval(game.timer, 1000);
+        game.qInterval = setInterval(game.qTimer, 1000);
 
         //Get user's question response
         $(".answers").on("click", function() {
+            clearInterval(game.qInterval);
             game.response = $(this).text();
-            console.log(game.response);
-        });
+            game.checkResponse();
+        })
+        },
 
-        //If the user responds, get response
-        //check the response vs answer
-        if (game.response!==''){
-            game.checkResponse(answer,game.response);
-        }
-    },
 
     //Play game function
     play: function() {
 
-        //Show questions and answers display
+        console.log("new game started from play function");
+
+        //Set game variables back to zero if user clicks on the
+        //home page Test Your 80s Knowledge button again after already playing
+        game.correctCounter = 0;
+        game.wrongCounter = 0;
+        game.unanswered = 0;
+        game.qTime = 0;
+        game.response = '';
+        game.qInterval = 0;
+        game.dInterval = 0;
+        game.qNum = 0;
+        game.answer = '';
+
+        //Ensure both question and display intervals are stopped
+        clearInterval(game.qInterval);
+        clearInterval(game.dInterval);
+
+        //Show questions, qmessage and answers display
         $(".qa").css("display","block");
 
-        //Move home page display to the left
-        $(".home").animate({
-            "marginLeft":"-=150px",
-            "marginRight":"20px",
-        });
+        //Hide and reset scores section to ensure it's not showing
+        $(".correct").text("Correct Answers: " + game.correctCounter);
+        $(".wrong").text("Wrong Answers: " + game.wrongCounter);
+        $(".unanswered").text("Unanswered Questions: " + game.unanswered);
+        $(".scores").css("display","none");
 
-        //Start questions loop
-        game.qloop();
-
+        //Start questions
+        game.questions(game.qNum);
     }
 
 } //End of game object declaration
@@ -117,4 +188,4 @@ var game = {
 //Main program block
 $(document).ready(function() {
     $(".btn").click(game.play);
-});
+    })
